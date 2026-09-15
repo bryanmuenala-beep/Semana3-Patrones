b30a011590d59a0efa9e518e26e4ac4ab127cc0c
Bryan Muenala <bryan.muenala@uees.edu.ec>
2026-09-14 21:21:04 -0500

    refactor: extraer metodos de notificacion en DemoFactory

--- src/main/java/edu/uees/patrones/factory/DemoFactory.java
+++ src/main/java/edu/uees/patrones/factory/DemoFactory.java
@@ -3,30 +3,41 @@ package edu.uees.patrones.factory;
 public class DemoFactory {
 
     public static void main(String[] args) {
+        enviarCorreo();
+        enviarSMS();
+        enviarPush();
+        enviarWhatsApp();
+    }
 
+    private static void enviarCorreo() {
         CreadorNotificador creadorCorreo = new CreadorCorreo();
         creadorCorreo.enviarNotificacion(
-            "estudiante@uees.edu.ec",
-            "Su tutoria ha sido confirmada."
+                "estudiante@uees.edu.ec",
+                "Su tutoria ha sido confirmada."
         );
+    }
 
+    private static void enviarSMS() {
         CreadorNotificador creadorSMS = new CreadorSMS();
         creadorSMS.enviarNotificacion(
-            "0999999999",
-            "Recordatorio: tiene una tutoria programada."
+                "0999999999",
+                "Recordatorio: tiene una tutoria programada."
         );
+    }
 
+    private static void enviarPush() {
         CreadorNotificador creadorPush = new CreadorPush();
         creadorPush.enviarNotificacion(
-            "Estudiante",
-            "Su tutor ha actualizado el horario de la tutoria."
+                "Estudiante",
+                "Su tutor ha actualizado el horario de la tutoria."
         );
+    }
 
+    private static void enviarWhatsApp() {
         CreadorNotificador creadorWhatsApp = new CreadorWhatsApp();
-
-creadorWhatsApp.enviarNotificacion(
-    "0988888888",
-    "Su reserva de tutoria ha sido registrada correctamente."
-);
+        creadorWhatsApp.enviarNotificacion(
+                "0988888888",
+                "Su reserva de tutoria ha sido registrada correctamente."
+        );
     }
 }
